255d67efc254f072fa8369128dbb894aef82a307
vishvas <visuhadiyal@gmail.com>
2026-01-01 00:11:55 +0530

    changed BE url

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = "https://customer-support-agent-yl4n.onrender.com/" // || 'http://localhost:8000';
 
 export const STORE_KNOWLEDGE = {
   name: "Lumina Gear",
